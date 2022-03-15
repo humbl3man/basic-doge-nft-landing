2a8868e8e92800c9798031e0f96bf588ec886cd6
Konstantin M <humbl3dev@gmail.com>
2022-03-15 12:14:02 -0700

    wallet address display and copy

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -1,13 +1,36 @@
 import { motion } from 'framer-motion';
-import { useContext } from 'react';
-import { MdAccountBalanceWallet as WalletIcon } from 'react-icons/md';
+import { useContext, useEffect, useRef, useState } from 'react';
+import {
+  MdAccountBalanceWallet as WalletIcon,
+  MdFileCopy
+} from 'react-icons/md';
 import Context from '../state/context';
 
+let copyTimeout: number;
 // component to connect to metamask wallet
 const WalletConnect = () => {
   const { walletConnected, walletAddress, handleWalletConnect } =
     useContext(Context);
   const isMetamaskInstalled = Boolean(window.ethereum);
+  const addressInputRef = useRef<null | HTMLInputElement>(null);
+  const [addressCopied, setAddressCopied] = useState(false);
+
+  useEffect(() => {
+    if (!addressCopied) return;
+    copyTimeout = window.setTimeout(() => {
+      setAddressCopied(false);
+      window.clearTimeout(copyTimeout);
+    }, 5000);
+
+    return () => {
+      window.clearTimeout(copyTimeout);
+    };
+  }, [addressCopied]);
+
+  function handleCopyClick() {
+    navigator.clipboard.writeText(addressInputRef.current?.value ?? '');
+    setAddressCopied(true);
+  }
 
   if (!isMetamaskInstalled) {
     return (
@@ -19,8 +42,26 @@ const WalletConnect = () => {
 
   if (walletConnected && walletAddress) {
     return (
-      <div className="text-slate-400">
-        Your wallet address: <span className="text-white">{walletAddress}</span>
+      <div className="flex relative items-center py-1 px-2 bg-zinc-700 rounded-lg">
+        <input
+          ref={addressInputRef}
+          type="text"
+          readOnly
+          className="text-sm text-white bg-transparent"
+          value={walletAddress}
+        />
+        <button
+          type="button"
+          onClick={handleCopyClick}
+          className="inline-block p-1 ml-2 text-xl text-white"
+        >
+          <MdFileCopy />
+        </button>
+        {addressCopied && (
+          <div className="absolute right-0 bottom-[-20px] text-xs text-zinc-300">
+            Address copied!
+          </div>
+        )}
       </div>
     );
   }
